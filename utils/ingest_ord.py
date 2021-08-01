cd669a251af267eb682f78980904f5e2f705973d
david graff <deg711@g.harvard.edu>
2021-08-01 15:10:24 -0400

    adding ray distribution scaffolding

--- utils/ingest_ord.py
+++ utils/ingest_ord.py
@@ -1,12 +1,32 @@
 import argparse
 from pathlib import Path
-from typing import Dict, List, Tuple
+import pprint
+from typing import Dict, Iterable, List, Optional, Tuple, Union
 
-from rdkit import Chem
+import ray
+from rdkit import Chem, RDLogger
+from tqdm import tqdm
 
 from ord_schema import message_helpers
 from ord_schema.proto import dataset_pb2
 from ord_schema.proto import reaction_pb2
+
+_TEST_MAX_REACTIONS = 10000
+
+RDLogger.DisableLog('rdApp.*') 
+
+try:
+    ray.init('auto')
+except ConnectionError:
+    ray.init()
+
+@ray.remote
+def _process_reaction_batch(
+    reactions: Iterable[reaction_pb2.Reaction], reactants: bool = False,
+) -> List[Tuple[str, List[str], List[str]]]:
+    return [
+        process_reaction(reaction, reactants) for reaction in reactions
+    ]
 
 def process_reaction(
     reaction: reaction_pb2.Reaction, reactants: bool = False
@@ -61,7 +81,9 @@ def process_reaction(
 
     return reaction.reaction_id, product_inchikeys, reactant_inchikeys
 
-def process_file(filepath: Path, ingest_reactants: bool = False) -> List[Dict]:
+def process_file(filepath: Union[str, Path],
+                 ingest_reactants: bool = False,
+                 max_reactions: Optional[int] = None) -> List[Dict]:
     """process all the reactions in single ORD data file to obtain a list of
     table rows corresponding to each reaction
 
@@ -71,7 +93,10 @@ def process_file(filepath: Path, ingest_reactants: bool = False) -> List[Dict]:
         the filepath of the data file
     ingest_reactants : bool, default=False
         whether or not to ingest reactants as well
-
+    max_reactions: Optional[int], default=None
+        the maximum number of reactions to process from the dataset. If None,
+        process all reactions
+        
     Returns
     -------
     rows : List[Dict]
@@ -90,7 +115,7 @@ def process_file(filepath: Path, ingest_reactants: bool = False) -> List[Dict]:
     # NOTE(degraff): maybe a single for-loop is faster than 2 comprehensions
     rows = [
         process_reaction(reaction, ingest_reactants)
-        for reaction in dataset.reactions
+        for reaction in tqdm(dataset.reactions[:max_reactions], 'Processing reactions')
     ]
     rows = [
         {'reaction_id': reaction_id,
@@ -108,14 +133,31 @@ def main():
     parser.add_argument('--ingest-reactants', default=False, 
                         action='store_true',
                         help='whether to ingest reactants as well as products')
+    parser.add_argument('--test', default=False, action='store_true',
+                        help='whether to run in testing mode')
+    parser.add_argument('--test-filenames', nargs='+',
+                        help='specific filenames of ORD data files with which to test the script.')
+    parser.add_argument('--max-rxns', type=int,
+                        help='the maximum number of reactions to process from each dataset. By default, process all reactions from each dataset. If the --test flag is passed, proecss _TEST_MAX_REACTIONS reactions by default.')
     args = parser.parse_args()
 
-    parent_dir = args.ord_data_repo / 'data'
-
     db_rows = []
+    if args.test:
+        max_rxns = args.max_rxns or _TEST_MAX_REACTIONS
+        for filename in args.test_filenames:
+            db_rows.extend(process_file(
+                filename, args.ingest_reactants, max_rxns
+            ))
+        
+        pprint.pprint(db_rows[:20], compact=True)
+        exit(0)
+
+    parent_dir = args.ord_data_repo / 'data'
     for data_dir in parent_dir.iterdir():
         for filepath in data_dir.iterdir():
-            db_rows.extend(process_file(filepath, args.ingest_reactants))
+            db_rows.extend(process_file(
+                filepath, args.ingest_reactants, args.max_rxns
+            ))
 
 
 if __name__ == '__main__':
