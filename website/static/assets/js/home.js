ea7b46f83dc0eb025c32735deb7a8e00717e17ba
Brennan Overhoff <brennanoverhoff@gmail.com>
2022-07-31 14:51:47 -0400

    Updated the website README

--- website/static/assets/js/home.js
+++ website/static/assets/js/home.js
@@ -21,14 +21,6 @@ function getList(){
 
 getList();
 
-function generateData(cols,bounds){
-    let vals = [];
-    for(let i = 0; i < cols; i++){
-        vals.push(Math.random() * (bounds.max - bounds.min) + bounds.min);
-    }
-    return vals;
-}
-
 function renderBiosigs(desired, top){
     let desList = [];
     let t1 = [];
--- website/static/assets/js/summary.js
+++ website/static/assets/js/summary.js
@@ -42,14 +42,6 @@ function search(query){
     });
 }
 
-function generateData(cols,bounds){
-    let vals = [];
-    for(let i = 0; i < cols; i++){
-        vals.push(Math.random() * (bounds.max - bounds.min) + bounds.min);
-    }
-    return vals;
-}
-
 function renderBiosig(cmpdName,inchikey,cmpdBiosig,desired){
     bioList = [];
     desList = [];
